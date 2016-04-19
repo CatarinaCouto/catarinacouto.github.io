c49d71cc446f49fe015aca3a7634da089e79a843
unknown <catarcouto@live.com.pt>
2016-04-19 23:29:42 +0100

    Rearranjos gerais. Css + Página inicial.

--- js/catarinacouto.js
+++ js/catarinacouto.js
@@ -1,5 +1,3 @@
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -19,12 +17,3 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
-
-//$(function() {
-//	var totalCount = 3;
-  // var num =  Math.ceil( Math.random() * totalCount );
-//document.getElementsByTagName('header').background = 'img/catarina_'+num+'.jpg';
-//document.getElementsByTagName('header').style.backgroundRepeat = "no-repeat";
-//});
-
-
